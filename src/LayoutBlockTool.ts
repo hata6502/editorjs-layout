5d9cd3f630c8139d3301ba63fd1881af9a057601
Victor Mckenzie <djcoderedpro@gmail.com>
2023-05-29 22:03:27 -0400

    adding css parsing for styling. Cleaning up styling

--- src/LayoutBlockTool.ts
+++ src/LayoutBlockTool.ts
@@ -1,65 +1,62 @@
+
+require('./index.css').toString();
 import type EditorJS from "@editorjs/editorjs";
 import type {
-  BlockTool,
-  BlockToolConstructorOptions,
-  EditorConfig,
+    BlockTool,
+    BlockToolConstructorOptions,
+    EditorConfig,
 } from "@editorjs/editorjs";
-import { renderContainer } from "./container";
-import type {
-  LayoutBlockContainerData,
-  ValidatedLayoutBlockContainerData,
+import {
+    renderContainer
 } from "./container";
 import type {
-  LayoutBlockItemContentData,
-  ValidatedLayoutBlockItemContentData,
+    LayoutBlockContainerData,
+    ValidatedLayoutBlockContainerData,
+} from "./container";
+import type {
+    LayoutBlockItemContentData,
+    ValidatedLayoutBlockItemContentData,
 } from "./itemContent";
-
 interface LayoutBlockToolConfig {
-  EditorJS: typeof EditorJS;
-  editorJSConfig: Omit<
-    EditorConfig,
-    "holder" | "data" | "minHeight" | "readOnly"
-  >;
-  /** Reserved flag for the future */
-  enableLayoutEditing: false;
-  enableLayoutSaving: boolean;
-  initialData: ValidatedLayoutBlockToolData;
+    EditorJS: typeof EditorJS;
+    editorJSConfig: Omit <
+        EditorConfig,
+    "holder" | "data" | "minHeight" | "readOnly" >
+    ;
+    /** Reserved flag for the future */
+    enableLayoutEditing: false;
+    enableLayoutSaving: boolean;
+    initialData: ValidatedLayoutBlockToolData;
 }
-
 interface LayoutBlockToolData {
-  itemContent: LayoutBlockItemContentData;
-  layout?: LayoutBlockContainerData;
+    itemContent: LayoutBlockItemContentData;
+    layout ? : LayoutBlockContainerData;
 }
-
 interface ValidatedLayoutBlockToolData extends LayoutBlockToolData {
-  itemContent: ValidatedLayoutBlockItemContentData;
-  layout?: ValidatedLayoutBlockContainerData;
+    itemContent: ValidatedLayoutBlockItemContentData;
+    layout ? : ValidatedLayoutBlockContainerData;
 }
-
 interface LayoutBlockToolDispatchData {
-  (
-    action: (prevData: {
-      itemContent: LayoutBlockItemContentData;
-      layout: LayoutBlockContainerData;
-    }) => {
-      itemContent: LayoutBlockItemContentData;
-      layout: LayoutBlockContainerData;
-    }
-  ): void;
+    (
+        action: (prevData: {
+            itemContent: LayoutBlockItemContentData;
+            layout: LayoutBlockContainerData;
+        }) => {
+            itemContent: LayoutBlockItemContentData;
+            layout: LayoutBlockContainerData;
+        }
+    ): void;
 }
-
 class LayoutBlockTool implements BlockTool {
-  static get isReadOnlySupported() {
-    return true;
-  }
-
-  static get shortcut() {
-    return "";
-  }
-
-  static get toolbox() {
-    return {
-      icon: `
+    static get isReadOnlySupported() {
+        return true;
+    }
+    static get shortcut() {
+        return "";
+    }
+    static get toolbox() {
+        return {
+            icon: `
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512">
           <rect x="48" y="48" width="176" height="176" rx="20" ry="20" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/>
           <rect x="288" y="48" width="176" height="176" rx="20" ry="20" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/>
@@ -67,179 +64,168 @@ class LayoutBlockTool implements BlockTool {
           <rect x="288" y="288" width="176" height="176" rx="20" ry="20" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/>
         </svg>
       `,
-      title: "Layout",
-    };
-  }
-
-  #config!: LayoutBlockToolConfig;
-  #readOnly: boolean;
-  #wrapper: HTMLDivElement;
-  #wrapper_class: HTMLInputElement;
-  #api : any;
-  #itemContent: LayoutBlockItemContentData;
-  #layout: LayoutBlockContainerData;
-
-  constructor({
-    config,
-    data,
-    api,
-    readOnly,
-  }: BlockToolConstructorOptions<LayoutBlockToolData, LayoutBlockToolConfig>) {
-    this.#readOnly = readOnly;
-    this.#wrapper = document.createElement("div");
-    this.#wrapper_class = document.createElement("input");
-
-    this.#itemContent = {};
-    this.#api = api;
-    this.#layout = {
-      type: "container",
-      id: "",
-      className: "",
-      style: "",
-      children: [],
-    };
-
-    // Filter undefined and empty object.
-    // See also: https://github.com/codex-team/editor.js/issues/1432
-    if (config && "EditorJS" in config) {
-      this.#config = config;
-      this.#itemContent = config.initialData.itemContent;
-
-      if (config.initialData.layout) {
-        this.#layout = config.initialData.layout;
-      }
+            title: "Layout",
+        };
     }
-
-    // Filter undefined and empty object.
-    // See also: https://github.com/codex-team/editor.js/issues/1432
-    if (data && "itemContent" in data) {
-      this.#itemContent = data.itemContent;
-
-      if (data.layout) {
-        this.#layout = data.layout;
-      }
-    }
-
-  }
-
-  render() {
-    this.renderWrapper();
-
-    return this.#wrapper;
-  }
-
-  renderSettings(){
-    var _layout = this.#layout;
-    var _wrapper = this.#wrapper;
-    var _dispatch = this.#dispatchData;
-    var _itemContent = this.#itemContent;
-    var _this = this;
-    var _api = this.#api;
-
-    const settings = [
-      {
-        name: 'Add column',
-        icon: `<svg fill="#000000" width="20" height="20" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><title>plus-frame</title><path d="M0 26.016q0 2.496 1.76 4.224t4.256 1.76h20q2.464 0 4.224-1.76t1.76-4.224v-20q0-2.496-1.76-4.256t-4.224-1.76h-20q-2.496 0-4.256 1.76t-1.76 4.256v20zM4 26.016v-20q0-0.832 0.576-1.408t1.44-0.608h20q0.8 0 1.408 0.608t0.576 1.408v20q0 0.832-0.576 1.408t-1.408 0.576h-20q-0.832 0-1.44-0.576t-0.576-1.408zM8 16q0 0.832 0.576 1.44t1.44 0.576h4v4q0 0.832 0.576 1.408t1.408 0.576 1.408-0.576 0.608-1.408v-4h4q0.8 0 1.408-0.576t0.576-1.44-0.576-1.408-1.408-0.576h-4v-4q0-0.832-0.608-1.408t-1.408-0.608-1.408 0.608-0.576 1.408v4h-4q-0.832 0-1.44 0.576t-0.576 1.408z"></path></svg>`
-        ,button_click: function(){
-          _layout;
-          _dispatch;
-          _this;
-          
-          var id =  crypto.randomUUID();
-          _layout.children.push({
-            type: "item",
+    #config!: LayoutBlockToolConfig;
+    #readOnly: boolean;
+    #wrapper: HTMLDivElement;
+    #wrapper_class: HTMLInputElement;
+    #api: any;
+    #itemContent: LayoutBlockItemContentData;
+    #layout: LayoutBlockContainerData;
+    constructor({
+        config,
+        data,
+        api,
+        readOnly,
+    }: BlockToolConstructorOptions < LayoutBlockToolData, LayoutBlockToolConfig > ) {
+        this.#readOnly = readOnly;
+        this.#wrapper = document.createElement("div");
+        this.#wrapper_class = document.createElement("input");
+        this.#itemContent = {};
+        this.#api = api;
+        this.#layout = {
+            type: "container",
             id: "",
             className: "",
             style: "",
-            itemContentId: id});
-
-          _itemContent[id] = JSON.parse('{"blocks":[{"type":"paragraph","data":{"text":"Start typing here"}}]}');
-
-          _this.rerender();
-          _api.toolbar.close();
-
+            children: [],
+        };
+        // Filter undefined and empty object.
+        // See also: https://github.com/codex-team/editor.js/issues/1432
+        if (config && "EditorJS" in config) {
+            this.#config = config;
+            this.#itemContent = config.initialData.itemContent;
+            if (config.initialData.layout) {
+                this.#layout = config.initialData.layout;
+            }
         }
-      }
-    ];
-    const wrapper = document.createElement('div');
-
-    settings.forEach( tune => {
-      let button = document.createElement('div');
-
-      button.classList.add('cdx-settings-button');
-      button.innerHTML = tune.icon;
-      wrapper.appendChild(button);
-
-
-      button.addEventListener('click', () => {
-        tune.button_click();
+        // Filter undefined and empty object.
+        // See also: https://github.com/codex-team/editor.js/issues/1432
+        if (data && "itemContent" in data) {
+            this.#itemContent = data.itemContent;
+            if (data.layout) {
+                this.#layout = data.layout;
+            }
+        }
+    }
+    render() {
+        this.renderWrapper();
+        return this.#wrapper;
+    }
+    renderSettings() {
+        var _layout = this.#layout;
+        var _wrapper = this.#wrapper;
+        var _dispatch = this.#dispatchData;
+        var _itemContent = this.#itemContent;
+        var _this = this;
+        var _api = this.#api;
+        const settings = [{
+            name: 'Add column',
+            icon: `<svg fill="#000000" width="20" height="20" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><title>plus-frame</title><path d="M0 26.016q0 2.496 1.76 4.224t4.256 1.76h20q2.464 0 4.224-1.76t1.76-4.224v-20q0-2.496-1.76-4.256t-4.224-1.76h-20q-2.496 0-4.256 1.76t-1.76 4.256v20zM4 26.016v-20q0-0.832 0.576-1.408t1.44-0.608h20q0.8 0 1.408 0.608t0.576 1.408v20q0 0.832-0.576 1.408t-1.408 0.576h-20q-0.832 0-1.44-0.576t-0.576-1.408zM8 16q0 0.832 0.576 1.44t1.44 0.576h4v4q0 0.832 0.576 1.408t1.408 0.576 1.408-0.576 0.608-1.408v-4h4q0.8 0 1.408-0.576t0.576-1.44-0.576-1.408-1.408-0.576h-4v-4q0-0.832-0.608-1.408t-1.408-0.608-1.408 0.608-0.576 1.408v4h-4q-0.832 0-1.44 0.576t-0.576 1.408z"></path></svg>`,
+            button_click: function() {
+                _layout;
+                _dispatch;
+                _this;
+                var id = crypto.randomUUID();
+                _layout.children.push({
+                    type: "item",
+                    id: "",
+                    className: "",
+                    style: "",
+                    itemContentId: id
+                });
+                _itemContent[id] = JSON.parse('{"blocks":[{"type":"paragraph","data":{"text":"Start typing here"}}]}');
+                _this.rerender();
+                _api.toolbar.close();
+            }
+        }];
+        const wrapper = document.createElement('div');
+        settings.forEach(tune => {
+            let button = document.createElement('div');
+            button.classList.add('cdx-settings-button');
+            button.innerHTML = tune.icon;
+            wrapper.appendChild(button);
+            button.addEventListener('click', () => {
+                tune.button_click();
+                return true;
+            });
+        });
+        return wrapper;
+    }
+    save(): LayoutBlockToolData {
+        return {
+            itemContent: this.#itemContent,
+            layout: this.#config.enableLayoutSaving ? this.#layout : undefined,
+        };
+    }
+    validate(data: LayoutBlockToolData) {
+        const compatibilityCheck: ValidatedLayoutBlockToolData = data;
         return true;
-      });
-
-    });
-
-    return wrapper;
-  }
-
-  save(): LayoutBlockToolData {
-    return {
-      itemContent: this.#itemContent,
-      layout: this.#config.enableLayoutSaving ? this.#layout : undefined,
+    }
+    #dispatchData: LayoutBlockToolDispatchData = (action) => {
+        const data = action({
+            itemContent: this.#itemContent,
+            layout: this.#layout,
+        });
+        this.#itemContent = data.itemContent;
+        this.#layout = data.layout;
+        this.renderWrapper();
     };
-  }
-
-  validate(data: LayoutBlockToolData) {
-    const compatibilityCheck: ValidatedLayoutBlockToolData = data;
-
-    return true;
-  }
-
-  #dispatchData: LayoutBlockToolDispatchData = (action) => {
-    const data = action({
-      itemContent: this.#itemContent,
-      layout: this.#layout,
-    });
-
-    this.#itemContent = data.itemContent;
-    this.#layout = data.layout;
-    this.renderWrapper();
-
-  };
-
-  rerender(){
-    this.renderWrapper();
-  }
-
-  renderWrapper() {
-    this.#wrapper.innerHTML = "";
-
-    var label  = document.createElement("label");
-    label.innerText = "Container class";
-
-    this.#wrapper_class.setAttribute("type", "text");
-    this.#wrapper_class.classList.add("wrapper_class");
-
-    label.append(this.#wrapper_class);
-
-    this.#wrapper.append(label);
-    this.#wrapper.append(
-      renderContainer({
-        EditorJS: this.#config.EditorJS,
-        data: this.#layout,
-        wrapper_class: this.#wrapper_class,
-        dispatchData: this.#dispatchData,
-        editorJSConfig: this.#config.editorJSConfig,
-        itemContentData: this.#itemContent,
-        readOnly: this.#readOnly,
-      })
-    );
-  }
+    rerender() {
+        this.renderWrapper();
+    }
+    renderWrapper() {
+        this.#wrapper.innerHTML = "";
+        var label = document.createElement("label");
+        this.#wrapper_class.setAttribute("placeholder", "Container class");
+        this.#wrapper_class.classList.add("cdx-input");
+        this.#wrapper_class.setAttribute("type", "text");
+        this.#wrapper_class.classList.add("wrapper_class");
+        this.#wrapper_class.addEventListener("keyup", (e) => {
+            this.#wrapper;
+            this.#itemContent;
+            this.#layout;
+            if (e.key.toLowerCase() == "meta" || e.altKey) {
+                return;
+            }
+            e = (e as KeyboardEvent);
+            if (!e.currentTarget) return;
+            var currentTarget = (e.currentTarget as HTMLInputElement);
+            var old_value = this.#layout.className;
+            this.#layout.className = currentTarget.value;
+            if (this.#wrapper.classList.contains(old_value)) {
+                if (currentTarget.value.length == 0) {
+                    this.#wrapper.classList.remove(old_value);
+                } else {
+                    this.#wrapper.classList.replace(old_value, this.#layout.className);
+                }
+            } else {
+                this.#wrapper.classList.add(this.#layout.className);
+            }
+        });
+        label.append(this.#wrapper_class);
+        this.#wrapper.append(label);
+        this.#wrapper.append(
+            renderContainer({
+                EditorJS: this.#config.EditorJS,
+                data: this.#layout,
+                wrapper_class: this.#wrapper_class,
+                dispatchData: this.#dispatchData,
+                editorJSConfig: this.#config.editorJSConfig,
+                itemContentData: this.#itemContent,
+                readOnly: this.#readOnly,
+            })
+        );
+    }
 }
-
-export { LayoutBlockTool };
+export {
+    LayoutBlockTool
+};
 export type {
-  LayoutBlockToolConfig,
-  LayoutBlockToolData,
-  LayoutBlockToolDispatchData,
-  ValidatedLayoutBlockToolData,
+    LayoutBlockToolConfig,
+    LayoutBlockToolData,
+    LayoutBlockToolDispatchData,
+    ValidatedLayoutBlockToolData,
 };
--- src/container.ts
+++ src/container.ts
@@ -1,150 +1,155 @@
-import { EditorJSLayoutError } from "./EditorJSLayoutError";
+import {
+    EditorJSLayoutError
+} from "./EditorJSLayoutError";
 import type {
-  LayoutBlockToolConfig,
-  LayoutBlockToolDispatchData,
+    LayoutBlockToolConfig,
+    LayoutBlockToolDispatchData,
 } from "./LayoutBlockTool";
-import { renderItem } from "./item";
-import type { LayoutBlockItemData, ValidatedLayoutBlockItemData } from "./item";
-import type { LayoutBlockItemContentData } from "./itemContent";
-
+import {
+    renderItem
+} from "./item";
+import type {
+    LayoutBlockItemData,
+    ValidatedLayoutBlockItemData
+} from "./item";
+import type {
+    LayoutBlockItemContentData
+} from "./itemContent";
 interface LayoutBlockContainerData {
-  type: "container";
-  id: Element["id"];
-  className: Element["className"];
-  style: CSSStyleDeclaration["cssText"];
-  children: (LayoutBlockContainerData | LayoutBlockItemData)[];
+    type: "container";
+    id: Element["id"];
+    className: Element["className"];
+    style: CSSStyleDeclaration["cssText"];
+    children: (LayoutBlockContainerData | LayoutBlockItemData)[];
 }
-
 interface ValidatedLayoutBlockContainerData extends LayoutBlockContainerData {
-  children: (
-    | ValidatedLayoutBlockContainerData
-    | ValidatedLayoutBlockItemData
-  )[];
+    children: ( |
+        ValidatedLayoutBlockContainerData |
+        ValidatedLayoutBlockItemData
+    )[];
 }
-
 interface RenderContext {
-  EditorJS: LayoutBlockToolConfig["EditorJS"];
-  dispatchData: LayoutBlockToolDispatchData;
-  editorJSConfig: LayoutBlockToolConfig["editorJSConfig"];
-  readOnly: boolean;
+    EditorJS: LayoutBlockToolConfig["EditorJS"];
+    dispatchData: LayoutBlockToolDispatchData;
+    editorJSConfig: LayoutBlockToolConfig["editorJSConfig"];
+    readOnly: boolean;
 }
-
 interface RenderContainerProps extends RenderContext {
-  data: LayoutBlockContainerData;
-  wrapper_class: HTMLInputElement;
-  itemContentData: LayoutBlockItemContentData;
+    data: LayoutBlockContainerData;
+    wrapper_class: HTMLInputElement;
+    itemContentData: LayoutBlockItemContentData;
 }
-
 const renderContainer = ({
-  data,
-  wrapper_class,
-  itemContentData,
-  ...context
+    data,
+    wrapper_class,
+    itemContentData,
+    ...context
 }: RenderContainerProps) => {
-  const wrapper = document.createElement("div");
-
-  wrapper.id = data.id;
-  wrapper.className = data.className;
-  wrapper.style.cssText = data.style;
-  var items = data.children;
-  
-  if( wrapper_class.value != undefined && wrapper_class.value ){
-    wrapper.className = wrapper_class.value;
-  }
-
-  data.children.forEach((child) => {
-    let childElement: HTMLDivElement;
-    switch (child.type) {
-      case "container": {
-        childElement = renderContainer({
-          wrapper_class,
-          ...context,
-          data: child,
-          itemContentData,
-        });
-
-        break;
-      }
-
-      case "item": {
-        childElement = renderItem({
-          ...context,
-          data: child,
-          itemContentData,
-        });
-
-        break;
-      }
-
-      default: {
-        const exhaustiveCheck: never = child;
-
-        throw new EditorJSLayoutError();
-      }
+    const wrapper = document.createElement("div");
+    wrapper.id = data.id;
+    wrapper.className = data.className;
+    wrapper.style.cssText = data.style;
+    var items = data.children;
+    if (wrapper_class.value != undefined && wrapper_class.value) {
+        wrapper.className = wrapper_class.value;
     }
-        
+    data.children.forEach((child) => {
+        let childElement: HTMLDivElement;
+        switch (child.type) {
+            case "container": {
+                childElement = renderContainer({
+                    wrapper_class,
+                    ...context,
+                    data: child,
+                    itemContentData,
+                });
+                break;
+            }
+            case "item": {
+                childElement = renderItem({
+                    ...context,
+                    data: child,
+                    itemContentData,
+                });
+                break;
+            }
+            default: {
+                const exhaustiveCheck: never = child;
+                throw new EditorJSLayoutError();
+            }
+        }
         child = (child as ValidatedLayoutBlockItemData);
-
         if (!child) return;
-        var itemContentId = !child.itemContentId  ? "" : child.itemContentId;
+        var itemContentId = !child.itemContentId ? "" : child.itemContentId;
         childElement.setAttribute("item-content-id", itemContentId);
+        var input = document.createElement("input");
+        var label = document.createElement("label");
+        input.classList.add("cdx-input");
+        input.setAttribute("placeholder", "Child Container class");
+        label.classList.add("d-flex");
+        label.classList.add("margin-x");
 
-        var input  = document.createElement("input");
-        var label  = document.createElement("label");
-        label.innerText = "Child Container class";
         input.setAttribute("type", "text");
-
         var button = document.createElement("button");
         button.innerHTML = "Remove container";
+        button.classList.add("cdx-button");
+        button.classList.add("bold");
+        button.classList.add("margin-left");
 
-        button.addEventListener("click", function(e){
-          items;
-          itemContentData;
-          childElement;
-
-          var target = (e.currentTarget as HTMLInputElement);
-
-          if (!target.parentElement) return;
-          if (!target.parentElement.parentElement) return;
-          if (!target.parentElement.parentElement.parentElement) return;
-          if (!target.parentElement.parentElement.parentElement.children) return;
-
-          //delete items index
-          var nodes = Array.prototype.slice.call(target.parentElement.parentElement.parentElement.children );
-          var index = nodes.indexOf(target.parentElement.parentElement);
-
-          items.splice(index,1);
-
-          delete itemContentData[itemContentId];
-
-          childElement.remove();
-
+        button.addEventListener("click", function(e) {
+            items;
+            itemContentData;
+            childElement;
+            var target = (e.currentTarget as HTMLInputElement);
+            if (!target.parentElement) return;
+            if (!target.parentElement.parentElement) return;
+            if (!target.parentElement.parentElement.parentElement) return;
+            if (!target.parentElement.parentElement.parentElement.children) return;
+            //delete items index
+            var nodes = Array.prototype.slice.call(target.parentElement.parentElement.parentElement.children);
+            var index = nodes.indexOf(target.parentElement.parentElement);
+            items.splice(index, 1);
+            delete itemContentData[itemContentId];
+            childElement.remove();
         });
-
         input.value = child.className;
-        input.addEventListener("keydown", (e) => {
-
-            child.className = child.className + e.key;
+        input.addEventListener("keyup", (e) => {
+            childElement;
+            e = (e as KeyboardEvent);
+            if (!e.currentTarget) return;
+            var currentTarget = (e.currentTarget as HTMLInputElement);
+            if (e.key.toLowerCase() == "meta" || e.altKey) {
+                return;
+            }
+            var old_value = child.className;
+            child.className = currentTarget.value;
+            if (childElement.classList.contains(old_value)) {
+                if (currentTarget.value.length == 0) {
+                    childElement.classList.remove(old_value);
+                } else {
+                    childElement.classList.replace(old_value, child.className);
+                }
+            } else {
+                childElement.classList.add(child.className);
+            }
         });
-
-        if(child.className != undefined){
+        if (child.className != undefined) {
             input.value = child.className;
         }
-
         label.append(input);
         childElement.prepend(label);
         label.append(button);
+        wrapper.append(childElement);
 
-      wrapper.append(childElement);
-  });
-
-  return wrapper;
+    });
+    return wrapper;
 };
-
-export { renderContainer };
+export {
+    renderContainer
+};
 export type {
-  LayoutBlockContainerData,
-  RenderContainerProps,
-  RenderContext,
-  ValidatedLayoutBlockContainerData,
+    LayoutBlockContainerData,
+    RenderContainerProps,
+    RenderContext,
+    ValidatedLayoutBlockContainerData,
 };
